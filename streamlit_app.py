0c1ddf010950b47cdd0db0690c3fdfbb59786a77
ajay <83819613+majay777@users.noreply.github.com>
2024-04-17 14:45:57 +0530

    Adding information about selected fruit on ui

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,9 +26,14 @@ st.dataframe(data=my_dataframe,use_container_width=True )
 ingredients_list = st.multiselect("Choose up to 5 ingredients: ", my_dataframe, max_selections=5)
 
 if ingredients_list:
-    ingredient_string = ", ".join(ingredients_list)
-    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-    fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
+    ingredient_string = ""
+
+    for fruit_chosen in ingredients_list:
+        ingredient_string += fruit_chosen + ' '
+        st.subheader(fruit_chosen + 'Nutrition Information')
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_chosen)
+        fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
+        
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order) 
                      values('""" + ingredient_string   + """' ,'""" +  name_on_order +"""') """
     time_to_insert = st.button("Submit Order")
